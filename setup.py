77b49928c29ee99485f650f9d19d1aa934034bc0
manavmisra2 <manavmisra2@gmail.com>
2022-08-16 19:02:19 +0530

    latest

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open(pjoin(here, 'version.py')) as f:
 setup_args = dict(
     name                = 'quanturf',
     packages            = ['quanturf'],
-    version             = "3",
+    version             = "4",
     description         = """REMOTE_USER Authenticator: An Authenticator for Jupyterhub to read user information from HTTP request headers, as when running behind an authenticating proxy.""",
     long_description    = "",
     author              = "Carl (https://github.com/tatsath)",
