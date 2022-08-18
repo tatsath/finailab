85375cffdf940cac277e92d32203ad6a7f810681
manavmisra2 <manavmisra2@gmail.com>
2022-08-18 23:28:10 +0530

    Final commit git add .git add .

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ with open(pjoin(here, 'version.py')) as f:
 setup_args = dict(
     name='quanturf',
     packages=['quanturf'],
-    version="6",
+    version="7",
     description="""REMOTE_USER Authenticator: An Authenticator for Jupyterhub to read user information from HTTP request headers, as when running behind an authenticating proxy.""",
     long_description="",
     author="Carl (https://github.com/tatsath)",
@@ -55,7 +55,7 @@ setup_args = dict(
 # setuptools requirements
 if 'setuptools' in sys.modules:
     setup_args['install_requires'] = install_requires = []
-    install_requires.append('jupyterlab')
+    install_requires.append('jupyterlab3.4.4')
     install_requires.append('jupyterlab_templates')
 
 
